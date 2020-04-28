6879c97fdeec7a6cee1bdb20c0f148dd6867bb77
KolasKarpenko <kolas@ngs.ru>
2020-04-28 16:59:16 +0200

    linux build
    Apply building for linux

--- SqlRepoLib/ISqlValue.cpp
+++ SqlRepoLib/ISqlValue.cpp
@@ -5,14 +5,14 @@
 namespace repo
 {
 
-std::function<std::string(const char*)> SqlString::ms_convertFromData = [](const char* data) { return std::string(data); };
-std::function<double(const char*)> SqlDouble::ms_convertFromData = [](const char* data) { return std::stod(data); };
-std::function<int(const char*)> SqlInteger::ms_convertFromData = [](const char* data) { return std::stoi(data); };
-std::function<int64_t(const char*)> SqlBigInt::ms_convertFromData = [](const char* data) { return std::stoll(data); };
+template <> std::function<std::string(const char*)> SqlString::ms_convertFromData = [](const char* data) { return std::string(data); };
+template <> std::function<double(const char*)> SqlDouble::ms_convertFromData = [](const char* data) { return std::stod(data); };
+template <> std::function<int(const char*)> SqlInteger::ms_convertFromData = [](const char* data) { return std::stoi(data); };
+template <> std::function<int64_t(const char*)> SqlBigInt::ms_convertFromData = [](const char* data) { return std::stoll(data); };
 
-std::function<std::string(const std::string&)> SqlString::ms_convertToSqlValue = [](const std::string& data) { return Tools::GetTextValue(data); };
-std::function<std::string(const double&)> SqlDouble::ms_convertToSqlValue = [](const double& data) { return std::to_string(data); };
-std::function<std::string(const int32_t&)> SqlInteger::ms_convertToSqlValue = [](const int32_t& data) { return std::to_string(data); };
-std::function<std::string(const int64_t&)> SqlBigInt::ms_convertToSqlValue = [](const int64_t& data) { return std::to_string(data); };
+template <> std::function<std::string(const std::string&)> SqlString::ms_convertToSqlValue = [](const std::string& data) { return Tools::GetTextValue(data); };
+template <> std::function<std::string(const double&)> SqlDouble::ms_convertToSqlValue = [](const double& data) { return std::to_string(data); };
+template <> std::function<std::string(const int32_t&)> SqlInteger::ms_convertToSqlValue = [](const int32_t& data) { return std::to_string(data); };
+template <> std::function<std::string(const int64_t&)> SqlBigInt::ms_convertToSqlValue = [](const int64_t& data) { return std::to_string(data); };
 
 }
